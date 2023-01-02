f10f9e74d020d8d519f9ca75630893c28d3ba8fd
Ramavatar Meena <ramavtarmeena@MacBook-Air.local>
2023-01-02 13:29:36 +0530

    api fixed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 
 export default function Home() {
@@ -15,16 +16,43 @@ const getApi = async()=>{
 }
 useEffect(() => {
 getApi()
-});
+},[]);
   return (
-    <div style={{width:"100%",height:"100vh",backgroundColor:"red"}}>Home
+    <div style={{width:"100%",height:"auto",backgroundColor:"transparent", display:"flex"}}>
     {getData?.map((e, index)=>{
         return(
             <>
-            <p style={{ width: "100%", height: "20%" }}>{e.id}</p>
+
+            <Card class="card">
+  <Img src={e.avatar} alt={e.first_name} />
+  <div class="container">
+    <h4><b>{e.first_name} {e.last_name}</b></h4>
+    <p>{e.email}</p>
+  </div>
+</Card>
+
+            
             </>
         )
     })}
     </div>
   )
 }
+
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  border-radius: 5px;
+  margin:10px;
+    background-color:blue ;
+  &:hover{
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+
+  }
+
+`
+const Img = styled.img`
+border-radius: 5px 5px 0 0;
+
+`
